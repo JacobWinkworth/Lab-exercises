f6e301371a2ff0297fc13569eab036a1e20acc07
JacobWinkworth <jacobwinkworth1802@gmail.com>
2017-10-19 17:19:04 +0100

    Lab 6 exercises committed.

---
+++ Lab 6/items.py
@@ -0,0 +1,63 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "weight": 0.1,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "weight": 2,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "weight": 0.5,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "weight": 0.5,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "weight": 0.1,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "weight": 1,
+
+    "description": "This student handbook explains everything. Seriously."
+}
